5f4e175c4dc7f6037df82b6938f635b083a5b2c9
Robert Beckett <robert.beckett@onion.red>
2020-01-28 20:34:14 -0800

    Removed dummy test code from back-end main.

--- back-end/main.go
+++ back-end/main.go
@@ -1,19 +1,6 @@
 package main
 
-import (
-	"log"
-
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	_ "github.com/lib/pq"
-)
-
+// Placeholder
 func main() {
-	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=sap dbname=postgres password='password' sslmode=disable")
-	if err != nil {
-		panic(err)
-	}
-	log.Println("Connection Established")
-	db.Debug()
-	defer db.Close()
+
 }
