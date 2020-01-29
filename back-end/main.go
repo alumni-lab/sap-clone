24eadf28a83c7f6aa1665f9d9becd8912f44811a
Robert Beckett <robert.beckett@onion.red>
2020-01-28 17:55:11 -0800

    Created baseline for db schema.

--- back-end/main.go
+++ back-end/main.go
@@ -1,11 +1,19 @@
 package main
 
-import "github.com/jinzhu/gorm"
+import (
+	"log"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	db, err := gorm.Open("postgres", "test.db")
+	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=sap dbname=postgres password='password' sslmode=disable")
 	if err != nil {
-		panic("Failed to connect to database")
+		panic(err)
 	}
+	log.Println("Connection Established")
+	db.Debug()
 	defer db.Close()
 }
