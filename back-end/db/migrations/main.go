bf6177b94f7f1c26589dd55d0758ac110806df6b
Robert Beckett <robert.beckett@onion.red>
2020-01-28 19:02:53 -0800

    Finished schema and confirmed correctly interacting with local psql.

--- back-end/db/migrations/main.go
+++ back-end/db/migrations/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"../schema"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=sap dbname=postgres password='password' sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	log.Println("Connection Established")
+
+	schema.Migrate(db)
 }
