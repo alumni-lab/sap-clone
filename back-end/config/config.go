5eeb919467019f7c32f140f1550e7c1e54d520d9
Robert Beckett <robert.beckett@onion.red>
2020-01-31 17:15:49 -0800

    Accidentally ignored config.go, corrected.  Created and manually tested backend config program, will automatically read and assign env's for ANYTHING added under database in the config.yml

---
+++ back-end/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+var varTypes = [5]string{
+	"DBHost",
+	"DBName",
+	"DBPort",
+	"DBUser",
+	"DBPassword",
+}
+
+func retrieveDBEnvVariables() {
+	viper.SetConfigName("config")
+	// Could add multiple paths if neccesary
+	viper.AddConfigPath("../..")
+	viper.SetConfigType("yml")
+
+	// Maybe be able to use this to change
+	// the db live for faster testing.
+	// viper.WatchConfig()
+	// It also has an event handler for changes
+	// viper.OnConfigChange(func)
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("Error reading config file, %s", err)
+	}
+
+	dbVars := viper.Sub("database")
+
+	// Allows the program to automatically pick up
+	// new config variables under database
+	for i := range varTypes {
+		os.Setenv(varTypes[i], dbVars.GetString(varTypes[i]))
+	}
+}
