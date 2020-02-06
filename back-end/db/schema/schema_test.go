ba9b34db7e82a6943e70fdba19a24eeac2e653c8
Robert Beckett <robert.beckett@onion.red>
2020-02-05 17:04:30 -0800

    Created TestMigrate file and func sig.

---
+++ back-end/db/schema/schema_test.go
@@ -0,0 +1,11 @@
+package schema
+
+import (
+	"testing"
+)
+
+func TestMigrate(t *testing.T) {
+	// Create a dummy DB and run Migrate(db)
+
+	// Check that all tables migrate should create exist.
+}
