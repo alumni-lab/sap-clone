967d5cc6b74be113bb7643be6a68edbddc6557cf
edison-cy-yang <edison.cy.yang@gmail.com>
2020-03-10 15:23:32 -0700

    initial material table setup for vendor order list

--- front-end/src/components/PurchaseOrderItem.js
+++ front-end/src/components/PurchaseOrderItem.js
@@ -1,14 +1,29 @@
 import React from "react";
 import "../components/InventoryListItem.scss";
 
+// import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
 var classnames = require('classnames');
 
 export default function PurchaseOrderItem(props) {
   return (
-    <li className = "inventory-item">
-      <h3 >{props.POid}</h3>
-      <h2 >{props.customerName}</h2>
-      <h3 >{props.amount}</h3>
-    </li>
+    <TableRow key={props.key}>
+      <TableCell>
+        {props.POid}
+      </TableCell>
+      <TableCell>
+        {props.customerName}
+      </TableCell>
+      <TableCell>
+        {props.amount}
+      </TableCell>
+    </TableRow>
   );
 }
--- front-end/src/components/PurchaseOrderList.js
+++ front-end/src/components/PurchaseOrderList.js
@@ -2,6 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PurchaseOrderItem from "../components/PurchaseOrderItem.js";
 
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
 export default function PurchaseOrderList(props) {
   const POs = props.POs.map(PO => {
     return (
@@ -14,5 +23,27 @@ export default function PurchaseOrderList(props) {
     );
   });
 
-  return POs;
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>
+              Order ID
+            </TableCell>
+            <TableCell>
+              Customer Name
+            </TableCell>
+            <TableCell>
+              Amount
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {POs}
+        </TableBody>
+
+      </Table>
+    </TableContainer>
+  );
 }
